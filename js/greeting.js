29eec3674bbfc104f9879b3623eb943fb7e42c39
Paul Johnson <36737851+Paulj1989@users.noreply.github.com>
2021-02-24 10:32:47 -0600

    Update greeting format

--- js/greeting.js
+++ js/greeting.js
@@ -6,8 +6,8 @@ const hour = today.getHours();
 const name = ' Paul';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep!  ';
-const gree2 = 'Good morning!  ';
+const gree1 = 'Go to Sleep  ';
+const gree2 = 'Good morning  ';
 const gree3 = 'Good afternoon  ';
 const gree4 = 'Good evening,  ';
 const gree5 = 'Good evening,  ';
