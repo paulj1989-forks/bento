192d8b9750159eab989bbb879d11be76c2b64f07
Paul Johnson <36737851+Paulj1989@users.noreply.github.com>
2021-02-24 10:23:35 -0600

    Update API and latlong

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = 'b09995469d8ebb175d59fff0aaad9115';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 51.507351;
+  let longitude = -0.127758;
 
   getWeather(latitude, longitude);
 }
